a657be23870dddbd2dfa5036e821e70a340b0dca
liaozhaozhou <liaozhaozhou@ecyao.com>
2023-09-10 17:25:23 +0800

    待办事件接口修改

--- routes/task.js
+++ routes/task.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const db = require('../sqlHandle/getTask')
 
-router.get('/list', async function (req, res, next) {
-  const {status} = req.query
+router.post('/list', async function (req, res, next) {
+  const {status} = req.body
   console.log('status', status)
   try {
     await db.getTaskList([status]).then(results => {
@@ -22,15 +22,88 @@ router.get('/list', async function (req, res, next) {
 })
 
 router.post('/add', async function (req, res, next) {
-  const { event, finishTime } = req.body
-  const finish_time = new Date(finishTime)
-  console.log(finish_time)
+  const { event, expectTtime } = req.body
+  const expect_time = new Date(expectTtime)
+  console.log('expect_time', expect_time)
   try {
-    await db.addTask([event, new Date(), finish_time]).then(results => {
+    await db.addTask([event, new Date(), expect_time]).then(results => {
       console.log(results)
       res.send({
         answer: true,
       })
+    })
+  } catch (err) {
+    console.log(err)
+    res.send({
+      answer: false
+    })
+  }
+})
+
+router.post('/finish', async function (req, res, next) {
+  const { id } = req.body
+  console.log('id', id)
+  try {
+    await db.finishTask([new Date(), id]).then(results => {
+      console.log(results)
+      if (results.affectedRows > 0) {
+        res.send({
+          answer: true,
+        })
+      } else {
+        res.send({
+          answer: false
+        })
+      }
+    })
+  } catch (err) {
+    console.log(err)
+    res.send({
+      answer: false
+    })
+  }
+})
+
+router.post('/delete', async function (req, res, next) {
+  const { id } = req.body
+  console.log('id', id)
+  try {
+    await db.deleteTask([new Date(), id]).then(results => {
+      console.log(results)
+      if (results.affectedRows > 0) {
+        res.send({
+          answer: true,
+        })
+      } else {
+        res.send({
+          answer: false
+        })
+      }
+    })
+  } catch (err) {
+    console.log(err)
+    res.send({
+      answer: false
+    })
+  }
+})
+
+router.post('/delay', async function (req, res, next) {
+  const { id, delayTime } = req.body
+  const delay_time = new Date(delayTime)
+  console.log('id', id)
+  try {
+    await db.delayTask([delay_time, id]).then(results => {
+      console.log(results)
+      if (results.affectedRows > 0) {
+        res.send({
+          answer: true,
+        })
+      } else {
+        res.send({
+          answer: false
+        })
+      }
     })
   } catch (err) {
     console.log(err)
--- sqlHandle/getTask.js
+++ sqlHandle/getTask.js
@@ -1,16 +1,35 @@
 const { executeQuerySky } = require('./index.js')
 
 function getTaskList (params) {
-    const sql = 'select * from taskList where status= ?'
+    const sql = 'select * from taskList where status in (?)'
     return executeQuerySky(sql, params)
 }
 
 function addTask (params) {
-    const sql = 'insert into taskList (event,create_time, finish_time, status) values(?, ?, ?, 0)'
+    const sql = 'insert into taskList (event,create_time, expect_time, status) values(?, ?, ?, 0)'
     return executeQuerySky(sql, params)
 }
 
+function finishTask (params) {
+    const sql = 'update taskList set finish_time = ?, status = 1 where id in (?)'
+    return executeQuerySky(sql, params)
+}
+
+function deleteTask (params) {
+    const sql = 'update taskList set cancel_time = ?, status = 4 where id in (?)'
+    return executeQuerySky(sql, params)
+}
+
+function delayTask (params) {
+    const sql = 'update taskList set delay_time = ?, status = 3 where id in (?)'
+    return executeQuerySky(sql, params)
+}
+
+
 module.exports = {
     getTaskList,
-    addTask
+    addTask,
+    finishTask,
+    deleteTask,
+    delayTask
 }
